e75f99d48b615a29dca472bde9463e05c102b97b
MateoEcheverriPelaez <162764429+MateoEcheverriPelaez@users.noreply.github.com>
2024-03-16 11:38:41 -0500

    Update Calculadora.java
    Se suben cambios para prueba en aws

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,11 +3,7 @@ package co.edu.eafit.protobootapp;
 //This is a simple calculator class.
 public class Calculadora {
 
-    public Double cuadrado(Double numero) {
-        return numero * numero;
-    }
+    public Double cuadrado(Double numero) {return numero * numero;}
 
-    public Double cubo(Double numero) {
-        return numero * numero * numero;
-    }
+    public Double cubo(Double numero) {return numero * numero * numero;}
 }
